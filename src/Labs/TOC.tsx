422d7ecdaf862601b676c91601e0a6029514625a
Nishanth Gopinath <gopinath.ni@northeastern.edu>
2025-01-11 21:38:28 -0500

    added github link and name

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kambaz">Kambaz</Link></li>
+        <li><Link to="https://github.com/NishFish/kambaz-react-web-app" id="wd-github">My GitHub</Link></li>
       </ul>
     );
   }
