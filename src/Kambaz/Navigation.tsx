1304ad446f8dfecd01a8603fc6800bccbeb3a2a8
Nishanth Gopinath <gopinath.ni@northeastern.edu>
2025-01-19 14:40:19 -0500

    updated nav bar

--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -2,78 +2,119 @@ import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
-import { Link } from "react-router";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-bootstrap";
 
 export default function KambazNavigation() {
   return (
-    <div id="wd-kambaz-navigation" style={{ width: 110 }}
-      className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2 ">
-      <a id="wd-neu-link" target="_blank"
+    <div
+      id="wd-kambaz-navigation"
+      style={{ width: 110 }}
+      className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
+    >
+      <a
+        id="wd-neu-link"
+        target="_blank"
         href="https://www.northeastern.edu/"
-        className="list-group-item bg-black border-0 text-center" >
-        <img src="/images/NEU.png" width="65px" /></a>
+        className="list-group-item bg-black border-0 text-center"
+      >
+        <img src="/images/NEU.png" width="65px" alt="NEU Logo" />
+      </a>
 
       <br />
 
-      <Link to="/Kambaz/Account" id="wd-account-link"
-        className="list-group-item text-center border-0 bg-black text-white">
-        <FaRegCircleUser className="fs-1 text text-white" /><br />
-        Account </Link>
+      <Nav variant="pills" className="flex-column">
+        <Nav.Item>
+          <Nav.Link
+            as={NavLink}
+            to="/Kambaz/Account"
+            id="wd-account-link"
+            className="list-group-item text-center border-0 bg-black text-white"
+          >
+            <FaRegCircleUser className="fs-1 text-white" />
+            <br />
+            Account
+          </Nav.Link>
+        </Nav.Item>
 
-      <br />
+        <br />
 
-      <Link to="/Kambaz/Dashboard" id="wd-dashboard-link"
-        className="list-group-item text-center border-0
-               bg-black text-white">
-        <AiOutlineDashboard className="fs-1 text-danger" /><br />
-        Dashboard </Link>
+        <Nav.Item>
+          <Nav.Link
+            as={NavLink}
+            to="/Kambaz/Dashboard"
+            id="wd-dashboard-link"
+            className="list-group-item text-center border-0 bg-black text-white"
+          >
+            <AiOutlineDashboard className="fs-1 text-danger" />
+            <br />
+            Dashboard
+          </Nav.Link>
+        </Nav.Item>
 
-      <br />
+        <br />
 
-      <Link to="/Kambaz/Dashboard" id="wd-course-link"
-        className="list-group-item text-white
-               bg-black text-center border-0">
-        <LiaBookSolid className="fs-1 text-danger" /><br />
-        Courses </Link>
+        <Nav.Item>
+          <Nav.Link
+            as={NavLink}
+            to="/Kambaz/Dashboard"
+            id="wd-course-link"
+            className="list-group-item text-center border-0 bg-black text-white"
+          >
+            <LiaBookSolid className="fs-1 text-danger" />
+            <br />
+            Courses
+          </Nav.Link>
+        </Nav.Item>
 
-      <br />
+        <br />
 
-      <Link to="/Kambaz/Calendar" id="wd-calender-link"
-        className="list-group-item text-white
-               bg-black text-center border-0">
-        <IoCalendarOutline className="fs-1 text-danger" /><br />
-        Calender </Link>
+        <Nav.Item>
+          <Nav.Link
+            as={NavLink}
+            to="/Kambaz/Calendar"
+            id="wd-calendar-link"
+            className="list-group-item text-center border-0 bg-black text-white"
+          >
+            <IoCalendarOutline className="fs-1 text-danger" />
+            <br />
+            Calendar
+          </Nav.Link>
+        </Nav.Item>
 
-      <br />
+        <br />
 
-      <Link to="/Kambaz/Inbox" id="wd-inbox-link"
-        className="list-group-item text-white
-               bg-black text-center border-0">
-        <FaInbox className="fs-1 text-danger" /><br />
-        Inbox </Link>
+        <Nav.Item>
+          <Nav.Link
+            as={NavLink}
+            to="/Kambaz/Inbox"
+            id="wd-inbox-link"
+            className="list-group-item text-center border-0 bg-black text-white"
+          >
+            <FaInbox className="fs-1 text-danger" />
+            <br />
+            Inbox
+          </Nav.Link>
+        </Nav.Item>
 
-      <br />
+        <br />
 
-      <Link to="/Labs" id="wd-labs-link"
-        className="list-group-item text-white
-               bg-black text-center border-0">
-        <LiaCogSolid className="fs-1 text-danger" /><br />
-        Labs </Link>
+        <Nav.Item>
+          <Nav.Link
+            as={NavLink}
+            to="/Labs"
+            id="wd-labs-link"
+            className="list-group-item text-center border-0 bg-black text-white"
+          >
+            <LiaCogSolid className="fs-1 text-danger" />
+            <br />
+            Labs
+          </Nav.Link>
+        </Nav.Item>
 
-      <br />
+        <br />
+      </Nav>
     </div>
   );
 }
-
-
-/*
-    <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link to="/Kambaz/Account" id="wd-account-link">Account</Link><br/>
-      <Link to="/Kambaz/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link to="/Kambaz/Dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link to="/Kambaz/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link to="/Kambaz/Inbox" id="wd-inbox-link">Inbox</Link><br/><br/>
-      <h3><Link to="/Labs" id="wd-labs-link">Labs</Link><br/></h3>
-    </div>
-*/
