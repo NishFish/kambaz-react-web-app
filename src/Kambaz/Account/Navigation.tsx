397babe42a022da8a56e9ac4a1710a5601087145
Nishanth Gopinath <gopinath.ni@northeastern.edu>
2025-01-27 22:14:14 -0500

    completed account section

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,9 +1,37 @@
+import { ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function AccountNavigation() {
   return (
-    <div id="wd-account-navigation">
-      <Link to={`/Kambaz/Account/Signin`}  > Signin  </Link> <br/>
-      <Link to={`/Kambaz/Account/Signup`}  > Signup  </Link> <br/>
-      <Link to={`/Kambaz/Account/Profile`} > Profile </Link> <br/>
-    </div>
-);}
+    <ListGroup id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+      <ListGroup.Item className="p-0 border-0 active">
+        <Link
+          to="/Kambaz/Account/Signin"
+          id="wd-course-home-link"
+          className="btn btn-link w-100 text-start list-group-item border-0"
+        >
+          Signin
+        </Link>
+      </ListGroup.Item>
+
+      <ListGroup.Item className="p-0 border-0">
+        <Link
+          to="/Kambaz/Account/Signup"
+          id="wd-course-modules-link"
+          className="btn btn-link w-100 text-start text-danger list-group-item border-0"
+        >
+          Sign up
+        </Link>
+      </ListGroup.Item>
+
+      <ListGroup.Item className="p-0 border-0">
+        <Link
+          to="/Kambaz/Account/Profile"
+          id="wd-course-modules-link"
+          className="btn btn-link w-100 text-start text-danger list-group-item border-0"
+        >
+          Profile
+        </Link>
+      </ListGroup.Item>
+    </ListGroup>
+  );
+}
--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -1,20 +1,62 @@
+import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function Profile() {
   return (
+    <div id="wd-signin-screen">
+
+      <h1>Profile</h1>
+
+      <Form.Control defaultValue="gopinath.ni"
+        placeholder="username"
+        className="mb-2 wd-username" />
+
+      <Form.Control defaultValue="randompass123"
+        placeholder="password"
+        type="password"
+        className="mb-2 wd-password" />
+
+      <Form.Control defaultValue="Nishanth"
+        placeholder="First Name"
+        className="mb-2 wd-firstname" />
+
+      <Form.Control defaultValue="Gopinath"
+        placeholder="Last Name"
+        className="mb-2 wd-lastname" />
+
+      <Form.Control defaultValue="2000-01-01"
+        type="date"
+        id="wd-dob"
+        className="mb-2" />
+
+      <Form.Control defaultValue="gopinath.ni@northeastern.edu"
+        type="email"
+        id="wd-email"
+        className="mb-2" />
+
+      <Form.Group controlId="wd-role">
+        <Form.Select defaultValue="USER">
+          <option value="USER">User</option>
+          <option value="ADMIN">Admin</option>
+          <option value="FACULTY">Faculty</option>
+          <option value="STUDENT">Student</option>
+        </Form.Select>
+      </Form.Group> <br />
+
+      <Link id="wd-signout-btn "
+        to="/Kambaz/Account/Signin"
+        className="btn btn-danger w-100 mb-2">
+        Sign out </Link>
+    </div>
+  );
+}
+
+/*
     <div id="wd-profile-screen">
-      <h3>Profile</h3>
-      <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
-      <input defaultValue="123"   placeholder="password" type="password"
-             className="wd-password" /><br/>
-      <input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>
-      <input defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br/>
-      <input defaultValue="2000-01-01" type="date" id="wd-dob" /><br/>
-      <input defaultValue="alice@wonderland" type="email" id="wd-email" /><br/>
       <select defaultValue="FACULTY" id="wd-role">
         <option value="USER">User</option>       <option value="ADMIN">Admin</option>
         <option value="FACULTY">Faculty</option> <option value="STUDENT">Student</option>
       </select><br/>
       <Link to="/Kambaz/Account/Signin" >Sign out</Link>
     </div>
-);}
+    */
