67e584139c59b0ae0ff9fbea665c9acb172e49f6
Nishanth Gopinath <gopinath.ni@northeastern.edu>
2025-02-13 15:50:44 -0500

    completed up to course navigation

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,72 +1,40 @@
 import { ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useParams, useLocation } from "react-router";
 
 export default function CourseNavigation() {
+  const links = [
+    { label: "Home", path: "/Home" },
+    { label: "Modules", path: "/Modules" },
+    { label: "Piazza", path: "/Piazza" },
+    { label: "Zoom", path: "/Zoom" },
+    { label: "Assignments", path: "/Assignments" },
+    { label: "Quizzes", path: "/Quizzes" },
+    { label: "Grades", path: "/Grades" },
+    { label: "People", path: "/People" },
+  ];
+
+  const { cid } = useParams();
+  const { pathname } = useLocation();
+
   return (
     <ListGroup id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <ListGroup.Item className="p-0 border-0 active">
-        <Link
-          to="/Kambaz/Courses/1234/Home"
-          id="wd-course-home-link"
-          className="btn btn-link w-100 text-start list-group-item border-0"
+      {links.map((link) => (
+        <ListGroup.Item
+          className={`p-0 border-0 ${pathname.includes(link.path) ? "active" : ""}`}
+          key={link.path}
         >
-          Home
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="p-0 border-0">
-        <Link
-          to="/Kambaz/Courses/1234/Modules"
-          id="wd-course-modules-link"
-          className="btn btn-link w-100 text-start text-danger list-group-item border-0"
-        >
-          Modules
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="p-0 border-0">
-        <Link
-          to="/Kambaz/Courses/1234/Piazza"
-          id="wd-course-piazza-link"
-          className="btn btn-link w-100 text-start text-danger list-group-item border-0"
-        >
-          Piazza
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="p-0 border-0">
-        <Link
-          to="/Kambaz/Courses/1234/Zoom"
-          id="wd-course-zoom-link"
-          className="btn btn-link w-100 text-start text-danger list-group-item border-0"
-        >
-          Zoom
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="p-0 border-0">
-        <Link
-          to="/Kambaz/Courses/1234/Assignments"
-          id="wd-course-assignments-link"
-          className="btn btn-link w-100 text-start text-danger list-group-item border-0"
-        >
-          Assignments
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="p-0 border-0">
-        <Link
-          to="/Kambaz/Courses/1234/Quizzes"
-          id="wd-course-quizzes-link"
-          className="btn btn-link w-100 text-start text-danger list-group-item border-0"
-        >
-          Quizzes
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="p-0 border-0">
-        <Link
-          to="/Kambaz/Courses/:cid/People"
-          id="wd-course-people-link"
-          className="btn btn-link w-100 text-start text-danger list-group-item border-0"
-        >
-          People
-        </Link>
-      </ListGroup.Item>
+          <Link
+            to={`/Kambaz/Courses/${cid}${link.path}`}
+            id={`wd-course-${link.label.toLowerCase()}-link`}
+            className={`btn btn-link w-100 text-start list-group-item border-0
+              ${pathname.includes(link.path) ? "text-dark" : "text-danger"}
+            `}
+          >
+            {link.label}
+          </Link>
+        </ListGroup.Item>
+      ))}
     </ListGroup>
   );
 }
--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -1,19 +1,22 @@
 import CourseNavigation from "./Navigation";
 import { Navigate, Route, Routes } from "react-router";
+import { courses } from "../Database";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
+import { useParams } from "react-router";
 
 
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
-        <FaAlignJustify className="me-4 fs-4 mb-1" />Course 101</h2>
-
+        <FaAlignJustify className="me-4 fs-4 mb-1" />{course && course.name}</h2>
       <hr />
 
       <div className="d-flex">
