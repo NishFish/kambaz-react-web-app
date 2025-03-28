e985bfe120ff4369f56cdde77ae8d83ab31fca1d
Nishanth Gopinath <gopinath.ni@northeastern.edu>
2025-03-28 19:44:36 -0400

    fixed deployment issues

--- src/Labs/Lab5/HttpClient.tsx
+++ src/Labs/Lab5/HttpClient.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import * as client from "./client";
 export default function HttpClient() {
-    const [welcomeOnClick, setWelcomeOnClick] = useState("");
+    //const [welcomeOnClick, setWelcomeOnClick] = useState("");
     const [welcomeOnLoad, setWelcomeOnLoad] = useState("");
-    const fetchWelcomeOnClick = async () => {
-        const message = await client.fetchWelcomeMessage();
-        setWelcomeOnClick(message);
-    };
+    //const fetchWelcomeOnClick = async () => {
+    //    const message = await client.fetchWelcomeMessage();
+    //    setWelcomeOnClick(message);
+    //}; #causing deployment issues
     const fetchWelcomeOnLoad = async () => {
         const welcome = await client.fetchWelcomeMessage();
         setWelcomeOnLoad(welcome);
