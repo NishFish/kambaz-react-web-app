60c828453122925882b446c238aa03f6f4fe8141
Nishanth Gopinath <gopinath.ni@northeastern.edu>
2025-04-05 15:15:25 -0400

    completed up till 6.2.6.7

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -7,20 +7,44 @@ let currentUser = null;
 
 export default function UserRoutes(app) {
     const createUser = (req, res) => { };
-    const deleteUser = (req, res) => { };
+
+    const deleteUser = async (req, res) => {
+        const status = await dao.deleteUser(req.params.userId);
+        res.json(status);
+    };
+    app.delete("/api/users/:userId", deleteUser);
+
     const findAllUsers = async (req, res) => {
+        const { role, name } = req.query;
+        if (role) {
+            const users = await dao.findUsersByRole(role);
+            res.json(users);
+            return;
+        }
+        if (name) {
+            const users = await dao.findUsersByPartialName(name);
+            res.json(users);
+            return;
+        }
         const users = await dao.findAllUsers();
         res.json(users);
     };
     app.get("/api/users", findAllUsers);
 
-    const findUserById = (req, res) => { };
+    const findUserById = async (req, res) => {
+        const user = await dao.findUserById(req.params.userId);
+        res.json(user);
+    };
+    app.get("/api/users/:userId", findUserById);
 
-    const updateUser = (req, res) => {
+    const updateUser = async (req, res) => {
         const userId = req.params.userId;
         const userUpdates = req.body;
-        dao.updateUser(userId, userUpdates);
-        const currentUser = dao.findUserById(userId);
+        await dao.updateUser(userId, userUpdates);
+        const currentUser = req.session["currentUser"];
+        if (currentUser && currentUser._id === userId) {
+            req.session["currentUser"] = { ...currentUser, ...userUpdates };
+        }
         req.session["currentUser"] = currentUser;
         res.json(currentUser);
     };
@@ -84,10 +108,10 @@ export default function UserRoutes(app) {
     };
     app.post("/api/users/current/courses", createCourse);
 
+
+
     app.post("/api/users", createUser);
-    app.get("/api/users/:userId", findUserById);
     app.put("/api/users/:userId", updateUser);
-    app.delete("/api/users/:userId", deleteUser);
     app.post("/api/users/signup", signup);
     app.post("/api/users/signin", signin);
     app.post("/api/users/signout", signout);
--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,5 +1,8 @@
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
+import PeopleDetails from "./Details";
+import { Link } from "react-router-dom";
+
 
 export default function PeopleTable({ users = [] }: { users?: any[] }) {
     return (
@@ -11,6 +14,8 @@ export default function PeopleTable({ users = [] }: { users?: any[] }) {
                 marginRight: "0.5rem",
             }}
         >
+            <PeopleDetails />
+
             <Table
                 striped
                 className="table-sm"
@@ -33,9 +38,11 @@ export default function PeopleTable({ users = [] }: { users?: any[] }) {
                         .map((user: any) => (
                             <tr key={user._id}>
                                 <td className="wd-full-name text-nowrap">
-                                    <FaUserCircle className="me-2 fs-1 text-secondary" />
-                                    <span className="wd-first-name">{user.firstName}</span>
-                                    <span className="wd-last-name">{user.lastName}</span>
+                                    <Link to={`/Kambaz/Account/Users/${user._id}`} className="text-decoration-none">
+                                        <FaUserCircle className="me-2 fs-1 text-secondary" />
+                                        <span className="wd-first-name">{user.firstName}</span>{" "}
+                                        <span className="wd-last-name">{user.lastName}</span>
+                                    </Link>
                                 </td>
                                 <td className="wd-login-id">{user.loginId}</td>
                                 <td className="wd-section">{user.section}</td>
